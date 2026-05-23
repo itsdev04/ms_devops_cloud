0eebd43e3b14eedf422a7913edb5aab994ee869b
itsdev04 <imkrdev@gmail.com>
2026-05-23 22:06:58 +0530

    config server ms setup

---
+++ devworks-config-server/src/main/java/com/devworks/DevworksConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.devworks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevworksConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevworksConfigServerApplication.class, args);
+	}
+
+}
